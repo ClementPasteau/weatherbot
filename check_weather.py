4b0681310be5d526b0906e5de315cadc01b977ca
Clément Pasteau <clementp@theodo.co.uk>
2017-07-23 23:23:30 +0100

    1st version of weatherbot script

--- check_weather.py
+++ check_weather.py
@@ -1,40 +1,39 @@
+# coding=utf-8
+import datetime
 import os
 import requests
+import json
 from slackclient import SlackClient
-from pyquery import PyQuery
-import datetime
-
-# starterbot's ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
+import locale
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(os.environ.get('SLACK_BOT_ID'))
+api_id = os.environ.get('WEATHER_API_ID')
+cities = [{'name': 'Paris', 'id': '6455259'}, {'name': 'Londres', 'id': '2643743'}]
 
-def check_drivers():
-    pre_url = 'http://www.guru3d.com/'
-    url = 'http://www.guru3d.com/files-categories/videocards-nvidia-geforce-vista-%7C-7.html'
-    headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0"}
-    forum_page = requests.get(url, headers=headers).content
-    pq = PyQuery(forum_page)
-    first_post_title = pq("h1")[0]
-    driver_version = first_post_title.text_content().split('driver')[0].strip()
-    print("driver version: %s" % driver_version)
-    date = pq(".newsstoryheader")[0].text_content().split('on:')[1].split('[')[0].strip()
-    print(date)
-    formatted_date = datetime.datetime.strptime(date, "%m/%d/%Y %I:%M %p")
-    date_difference = datetime.datetime.now() - formatted_date
-    seconds_difference = date_difference.seconds + date_difference.days * 86400
-    if (seconds_difference <= 1800):
-        print('New driver! Out %d minutes ago!' % (seconds_difference/60))
-        page_url = pre_url + first_post_title.getnext().getnext().find('a').get('href')
-        response = ":nvidia: @ryuken NEW DRIVER !! *%s* ! %s :nvidia:" % (driver_version, page_url)
-        slack_client.api_call("chat.postMessage", channel="general", text=response, as_user=True)
-    else:
-        print('Existing driver. Out %d minutes ago!' % (seconds_difference/60))
+def check_cities_weather():
+    for city in cities:
+        url = 'http://api.openweathermap.org/data/2.5/forecast?id=%s&APPID=%s&lang=fr' % (city['id'], api_id)
+        response = json.loads(requests.get(url).content)
+        message = 'prediction de pluie sur les prochaines 24h: %s' % city['name']
+        print(message)
+        slack_client.api_call("chat.postMessage", channel="general", text=message, as_user=True)
+        for index,element in enumerate(response['list']):
+            if (element['rain']) and index < 9:
+                quantity = int(element['rain']['3h'])
+                if (quantity > 1):
+                    time_split = element['dt_txt'].split(' ')
+                    time = time_split[1]
+                    parsed_date = datetime.datetime.strptime(time_split[0], "%Y-%m-%d")
+                    formatted_date = parsed_date.strftime('%d %B')
+                    message = '%s%% de pluie pour %s le %s' % (quantity, time, formatted_date)
+                    print(message)
+                    slack_client.api_call("chat.postMessage", channel="general", text=message, as_user=True)
 
 if __name__ == "__main__":
+    locale.setlocale(locale.LC_TIME, "fr_FR")
     if slack_client.rtm_connect():
-        print("NvidiaBot connected and running!")
-        check_drivers()
+        print("WeatherBot up and running!")
+        check_cities_weather()
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
